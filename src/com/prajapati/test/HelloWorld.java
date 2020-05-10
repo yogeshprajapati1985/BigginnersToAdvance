b319e134e0a3b601f38fa3ce5d1112e9830e3342
Yogesh <yogesh.prajapati1985@gmail.com>
2020-05-10 19:45:54 -0400

    Initial commit.

---
+++ src/com/prajapati/test/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.prajapati.test;
+
+/**
+ * @author Yogesh
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+
+}
