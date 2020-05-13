fdaaa270c6af12980b2bfaaece0bd777479ab502
Mansi Prajapati <65174549+immansi@users.noreply.github.com>
2020-05-13 14:57:52 +0530

    Program for online menu card of the hotels.

---
+++ Menu/src/Option.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+public class Option {
+	Scanner sc = new Scanner(System.in);
+	public void mainMenu() {
+		int select;
+		System.out.println("Welcome To The Mocha Restaurant");
+		System.out.println("\n Select From The Following Options According To Your Desire");
+		System.out.println("\n Let Us Know What Would You Like To have");
+		System.out.println("==========================================");
+		System.out.println("| 1. Cold Drinks |");
+		System.out.println("| 2. Coffee |");
+		System.out.println("| 3. Tea |");
+		System.out.println("| 4. Shakes |");		
+		System.out.println("| 5. Pizza |");
+		System.out.println("| 6. Sandwitch |");
+		
+		System.out.println("===========================================");
+		System.out.println("Please Select The Option Now");
+		
+		select = sc.nextInt();
+		
+		switch(select) {
+		case 1:
+			coldDrinks();
+			break;
+		case 2:
+			coffee();
+			break;
+		case 3:
+			tea();
+			break;
+		case 4:
+			shakes();
+			break;
+		case 5:
+			pizza();
+			break;
+		case 6:
+			sandwitch();
+			break;
+		case 7:
+			System.out.println("Thank you for visiting");
+			break;
+			
+		}
+	}
+public void coldDrinks() {
+	//int select1;
+	System.out.println("You have selected cold drings.");
+	System.out.println("Here are the available cold drinks");
+	System.out.println("| 1. Mojito Mocktail |");
+	System.out.println("| 2. Coca cola |");
+	System.out.println("| 3. Lemonade |");
+	System.out.println("| 4. Nectar |");
+}
+public void coffee() {
+	System.out.println("You have selected Coffee");
+	System.out.println("Here are the available Coffee");
+	System.out.println("| 1. cold coffee |");
+	System.out.println("| 2. Latte |");
+	System.out.println("| 3. Espresso |");
+	System.out.println("| 4. Cappuccino |");
+}
+public void tea() {
+	System.out.println("You have selected Tea");
+	System.out.println("Here are the available Tea");
+	System.out.println("| 1. Green Tea |");
+	System.out.println("| 2. Lemon Tea |");
+	System.out.println("| 3. Ice Tea |");
+	System.out.println("| 4. Black Tea|");
+}
+public void shakes() {
+	System.out.println("You have selected Shakes");
+	System.out.println("Here are the availale Shakes");
+	System.out.println("| 1. Orea Shake |");
+	System.out.println("| 2. Kitkate Shake |");
+	System.out.println("| 3. Nutella Shake |");
+	System.out.println("| 4. Ferrero Rocher Shake |");
+	
+}
+public void pizza() {
+	System.out.println("You have selected Pizza");
+	System.out.println("Here are the available Pizza");
+	System.out.println("| 1. Margareta Pizza |");
+	System.out.println("| 2. Paneer Parcel |");
+	System.out.println("| 3. Seven Cheezy |");
+	System.out.println("| 4. Onion N Capsicum|");
+}
+public void sandwitch() {
+	System.out.println("You have selected Sandwitch");
+	System.out.println("Here are the available Sandwitch");
+	System.out.println("| 1. Vegitable Sandwitch |");
+	System.out.println("| 2. Plain alloo matar Sandwitch |");
+	System.out.println("| 3. Club Sandwitch |");
+	System.out.println("| 4. Chocolate slice |");
+
+}
+
+	public static void main(String[] args) {
+		Option a = new Option();
+		a.mainMenu();
+
+	}
+
+}
