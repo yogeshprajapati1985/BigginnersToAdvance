0f9aa3891d5035aed81cff57354ec9248fdbc3fc
immansi <mansiprajapati.op@gmail.com>
2020-05-14 17:03:44 +0530

    changed the name of method
    changed the name of the method from get() to check().

--- prime/src/prime/demo.java
+++ prime/src/prime/demo.java
@@ -2,23 +2,23 @@ package prime;
 import java.util.Scanner;
 class First{
 	
-	public void get() {
+	public void check() {
 	Scanner sc = new Scanner(System.in);
 	 System.out.println("This is the program for checking the number whether it is prime or not");
 	 System.out.println("======================================================================");
 	 System.out.println("Enter the number you wanna check");
-	 int n = sc.nextInt();
+	 int n = sc.nextInt();  //store the number to check
 	 if(n==0 || n==1) 
 		 System.out.println("The number is not prime number");
 	 else {
-		 for(int i=2;i<n;i++) {
+		 for(int i=2;i<n;i++) {		
 			 if(n%i==0) {
 				 System.out.println("The number is not prime number");
-			 	return ;
+			 	 return ;	//returns when the number is not prime
 			 }
 			 else {
 				 System.out.println("The number is prime number");
-			     return;
+			    	 return ;	//returns when the number is prime
 			 }
 		 }
 	}
@@ -28,8 +28,8 @@ class First{
 public class demo extends First{
 
 	public static void main(String[] args) {
-			 First a= new First();
-		a.get();
+		First a= new First();
+		a.check();
 		System.out.println("======================================================================17");
 	}
 
